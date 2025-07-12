c6ca858dad4c2e968431acb47fee8a926d6a4228
ahmedanwar1234 <ahmedanswar123@gmail.com>
2025-07-12 03:03:47 +0300

    for deploy

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../libs/authOptions";
 
 
 import prisma from "../libs/prismadb";
