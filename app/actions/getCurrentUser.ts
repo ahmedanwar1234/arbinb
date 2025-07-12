dc0325719c53c955bd6d606feaf34f079990a8c0
ahmedanwar1234 <ahmedanswar123@gmail.com>
2025-07-12 03:09:14 +0300

    for deploy

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,36 +1,38 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../libs/authOptions";
 
-
 import prisma from "../libs/prismadb";
 
-
 export async function getSession() {
-    return await getServerSession(authOptions)
+  return await getServerSession(authOptions);
 }
 
-export default async function getCurrentUser(){
-    try {
-        const session=await getSession();
-      
-        if(!session?.user?.email){
-            return null
-        }
-        const currentUser=await prisma.user.findUnique({
-            where:{email:session.user.email as string}
-        })
-        if(!currentUser){
-            return null
-        }
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
 
-        return {...currentUser,
-            createdAt:currentUser.createdAt.toISOString(),
-            updatedAt:currentUser.updatedAt.toISOString(),
-            emailVerified:currentUser.emailVerified?.toISOString()||null,
-
-        
-        }
-    } catch (error) {
-        
+    if (!session?.user?.email) {
+      return null;
     }
+
+    const currentUser = await prisma.user.findUnique({
+      where: { email: session.user.email as string },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+      createdAt: new Date(currentUser.createdAt),
+      updatedAt: new Date(currentUser.updatedAt),
+      emailVerified: currentUser.emailVerified
+        ? new Date(currentUser.emailVerified)
+        : null,
+    };
+  } catch (error) {
+    console.error("Failed to get current user:", error);
+    return null;
+  }
 }
